b5c6720576bdc57770f6438ce85aca9bae59528b
Xiety <120028248+slentod@users.noreply.github.com>
2023-06-28 05:54:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Xiety'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Xiety:MeremMelek68@cluster0.ieukif3.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "txtnwkwk@gmail.com" //email
+global.email_password = "heeetdzngqdwfoub" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
